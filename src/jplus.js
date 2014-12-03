cea370ad35df55d3a5783cd79e38213520ad852e
Jade <guyingjie129@163.com>
2014-12-03 10:07:29 +0800

    初稿

--- src/jplus.js
+++ src/jplus.js
@@ -312,269 +312,6 @@
 		}
 	})
 
-	//scan
-
-	var $ = window.jQuery || window.Zepto
-
-	if ($ === undefined) {
-		return
-	}
-
-	var $plus = $.plus = {
-		attr: 'js',
-		filterAttr: ['noscan', 'app'],
-		viewModel: []
-	}
-	var push = calling(Array.prototype.push)
-
-	function Scaner($startNode) {
-		this.$startNode = $startNode
-		this.viewModel = {}
-	}
-
-	Scaner.prototype = {
-		filter: function($sources) {
-			var filterNode = []
-			var $startNode = this.$startNode
-
-			each($plus.filterAttr, function(attr) {
-				filterNode.push.apply(filterNode, slice($startNode.find('[' + attr + ']' + ' [' + $plus.attr + ']')))
-			})
-
-			return $sources.filter(function() {
-				return $.inArray(this, filterNode) === -1
-			})
-		},
-
-		parse: function($node) {
-			var viewModel = this.viewModel
-			each(_.parse($node.attr($plus.attr)), function(method, alias) {
-				var vm = viewModel[alias] = viewModel[alias] || {}
-				if (vm.instance) {
-					push(vm.instance, $node[0])
-				} else {
-					method = method.split('-')
-					vm['method'] = method[0]
-					vm['params'] = method[1] ? [method[1]] : []
-					vm['instance'] = $node
-					vm['lastValue'] = null
-				}
-			})
-			return this
-		},
-
-		scan: function() {
-
-			var that = this
-
-			this
-				.parse(this.$startNode)
-				.filter(this.$startNode.find('[' + $plus.attr + ']'))
-				.each(function() {
-					that.parse($(this))
-				})
-
-			return this
-		},
-
-		rescan: function() {
-			this.viewModel = {}
-			return this.scan()
-		},
-
-		get: function() {
-			return this.viewModel
-		}
-	}
-
-	$.fn.scanView = function(rescan) {
-		var elem = this[0]
-		var vmIndex = elem.vmIndex
-		var isNum = typeof vmIndex === 'number'
-		var vm
-		if (isNum) {
-			vm = $plus.viewModel[vmIndex]
-			return rescan ? vm.rescan().get() : vm.get()
-		}
-		vm = new Scaner(this)
-		$plus.viewModel[elem.vmIndex = $plus.viewModel.length] = vm
-		return vm.scan().get()
-	}
-
-
-	//refer to underscore
-	// Internal recursive comparison function for `isEqual`.
-	var eq = function(a, b, aStack, bStack) {
-		// Identical objects are equal. `0 === -0`, but they aren't identical.
-		// See the [Harmony `egal` proposal](http://wiki.ecmascript.org/doku.php?id=harmony:egal).
-		if (a === b) return a !== 0 || 1 / a === 1 / b;
-		// A strict comparison is necessary because `null == undefined`.
-		if (a == null || b == null) return a === b;
-		// Unwrap any wrapped objects.
-		// Compare `[[Class]]` names.
-		var className = toStr(a);
-		if (className !== toStr(b)) return false;
-		switch (className) {
-			// Strings, numbers, regular expressions, dates, and booleans are compared by value.
-			case '[object RegExp]':
-				// RegExps are coerced to strings for comparison (Note: '' + /a/i === '/a/i')
-			case '[object String]':
-				// Primitives and their corresponding object wrappers are equivalent; thus, `"5"` is
-				// equivalent to `new String("5")`.
-				return '' + a === '' + b;
-			case '[object Number]':
-				// `NaN`s are equivalent, but non-reflexive.
-				// Object(NaN) is equivalent to NaN
-				if (+a !== +a) return +b !== +b;
-				// An `egal` comparison is performed for other numeric values.
-				return +a === 0 ? 1 / +a === 1 / b : +a === +b;
-			case '[object Date]':
-			case '[object Boolean]':
-				// Coerce dates and booleans to numeric primitive values. Dates are compared by their
-				// millisecond representations. Note that invalid dates with millisecond representations
-				// of `NaN` are not equivalent.
-				return +a === +b;
-		}
-
-		var areArrays = className === '[object Array]';
-		if (!areArrays) {
-			if (typeof a != 'object' || typeof b != 'object') return false;
-
-			// Objects with different constructors are not equivalent, but `Object`s or `Array`s
-			// from different frames are.
-			var aCtor = a.constructor,
-				bCtor = b.constructor;
-			if (aCtor !== bCtor && !(isFn(aCtor) && aCtor instanceof aCtor &&
-					isFn(bCtor) && bCtor instanceof bCtor) && ('constructor' in a && 'constructor' in b)) {
-				return false;
-			}
-		}
-		// Assume equality for cyclic structures. The algorithm for detecting cyclic
-		// structures is adapted from ES 5.1 section 15.12.3, abstract operation `JO`.
-		var length = aStack.length;
-		while (length--) {
-			// Linear search. Performance is inversely proportional to the number of
-			// unique nested structures.
-			if (aStack[length] === a) return bStack[length] === b;
-		}
-
-		// Add the first object to the stack of traversed objects.
-		aStack.push(a);
-		bStack.push(b);
-		var size, result;
-		// Recursively compare objects and arrays.
-		if (areArrays) {
-			// Compare array lengths to determine if a deep comparison is necessary.
-			size = a.length;
-			result = size === b.length;
-			if (result) {
-				// Deep compare the contents, ignoring non-numeric properties.
-				while (size--) {
-					if (!(result = eq(a[size], b[size], aStack, bStack))) break;
-				}
-			}
-		} else {
-			// Deep compare objects.
-			var keys = _.keys(a),
-				key;
-			size = keys.length;
-			// Ensure that both objects contain the same number of properties before comparing deep equality.
-			result = _.keys(b).length === size;
-			if (result) {
-				while (size--) {
-					// Deep compare each member
-					key = keys[size];
-					if (!(result = hasOwn(b, key) && eq(a[key], b[key], aStack, bStack))) break;
-				}
-			}
-		}
-		// Remove the first object from the stack of traversed objects.
-		aStack.pop();
-		bStack.pop();
-		return result;
-	};
-
-	// Perform a deep comparison to check if two objects are equal.
-	_.isEqual = function(a, b) {
-		return eq(a, b, [], []);
-	};
-
-	var $fn = $.fn
-
-	function Sync(dataModel, viewModel) {
-		this.dataModel = dataModel
-		this.viewModel = viewModel
-	}
-
-	Sync.prototype = {
-		refresh: function() {
-			var that = this
-			var viewModel = this.viewModel
-			each(this.dataModel, function(data, key) {
-				if (!(key in viewModel)) {
-					return
-				}
-				that.render(viewModel[key], data)
-			})
-		},
-		render: function(vm, data) {
-			if (_.isEqual(data, vm.lastValue)) {
-				return
-			}
-			vm.lastValue = typeof data === 'object' ? isArr(data) ? data.concat() : extend(true, {}, data) : data
-
-			var instance = vm.instance
-			var method = vm.method
-			var params = vm.params
-			var inProto = vm.method in $fn
-
-			if (inProto) {
-
-				if (isSameType(data)) {
-					var template = instance.eq(0).clone()
-					var frag = []
-					method = $fn[method]
-					each(data, function(value, index) {
-						var $item = instance.eq(index)
-						if (!$item.length) {
-							$item = template.clone()
-							frag.push($item.get(0))
-						}
-						method.apply($item, params.concat(value))
-					})
-					if (frag.length) {
-						instance.eq(-1).after(frag)
-						frag.push.apply(instance, frag)
-					}
-				} else {
-					$fn[method].apply(instance, params.concat(data))
-				}
-
-			} else {
-
-				if (!isFn(data)) {
-					return
-				}
-				method = data
-				params = this.dataModel[vm.method]
-				method.apply(instance, [].concat(params || []))
-			}
-		}
-	}
-
-
-	$.fn.refresh = function(dataModel) {
-		var that = this
-		if (isObj(dataModel)) {
-			new Sync(dataModel, this.scanView()).refresh()
-		} else if (isArr(dataModel)) {
-			each(dataModel, function(dm, index) {
-				new Sync(dm, that.eq(index).scanView()).refresh()
-			})
-		}
-		return this
-	}
-
 	//observe.js
 
 	function throwErr(msg) {
@@ -899,97 +636,371 @@
 
 	createObserver.ES5 = false
 
+	//scan
+
+	var $ = window.jQuery || window.Zepto
+
+	if ($ === undefined) {
+		if (isFn(global.define) && (define.amd || define.cmd)) {
+			define({
+				agent: createProxy,
+				observe: createObserver
+			})
+		} else {
+			global.agent = createProxy
+			global.observe = createObserver
+		}
+		return
+	}
+
+	$.agent = createProxy
 	$.observe = createObserver
 
+	var $plus = $.plus = {
+		attr: 'js',
+		filterAttr: ['noscan', 'app'],
+		viewModel: []
+	}
+
+	var push = calling(Array.prototype.push)
+
+	function Scaner($startNode) {
+		this.$startNode = $startNode
+		this.viewModel = {}
+	}
+
+	Scaner.prototype = {
+		filter: function($sources) {
+			var filterNode = []
+			var $startNode = this.$startNode
+
+			each($plus.filterAttr, function(attr) {
+				filterNode.push.apply(filterNode, slice($startNode.find('[' + attr + ']' + ' [' + $plus.attr + ']')))
+			})
+
+			return $sources.filter(function() {
+				return $.inArray(this, filterNode) === -1
+			})
+		},
+
+		parse: function($node) {
+			var viewModel = this.viewModel
+			each(_.parse($node.attr($plus.attr)), function(method, alias) {
+				var vm = viewModel[alias] = viewModel[alias] || {}
+				if (vm.instance) {
+					push(vm.instance, $node[0])
+				} else {
+					method = method.split('-')
+					vm['method'] = method[0]
+					vm['params'] = method[1] ? [method[1]] : []
+					vm['instance'] = $node
+					vm['lastValue'] = null
+				}
+			})
+			return this
+		},
+
+		scan: function() {
+
+			var that = this
+
+			this
+				.parse(this.$startNode)
+				.filter(this.$startNode.find('[' + $plus.attr + ']'))
+				.each(function() {
+					that.parse($(this))
+				})
+
+			return this
+		},
+
+		rescan: function() {
+			this.viewModel = {}
+			return this.scan()
+		},
+
+		get: function() {
+			return this.viewModel
+		}
+	}
+
+	$.fn.scanView = function(rescan) {
+		var elem = this[0]
+		var vmIndex = elem.vmIndex
+		var isNum = typeof vmIndex === 'number'
+		var vm
+		if (isNum) {
+			vm = $plus.viewModel[vmIndex]
+			return rescan ? vm.rescan().get() : vm.get()
+		}
+		vm = new Scaner(this)
+		$plus.viewModel[elem.vmIndex = $plus.viewModel.length] = vm
+		return vm.scan().get()
+	}
+
+
+	//refer to underscore
+	// Internal recursive comparison function for `isEqual`.
+	var eq = function(a, b, aStack, bStack) {
+		// Identical objects are equal. `0 === -0`, but they aren't identical.
+		// See the [Harmony `egal` proposal](http://wiki.ecmascript.org/doku.php?id=harmony:egal).
+		if (a === b) return a !== 0 || 1 / a === 1 / b
+		// A strict comparison is necessary because `null == undefined`.
+		if (a == null || b == null) return a === b
+		// Unwrap any wrapped objects.
+		// Compare `[[Class]]` names.
+		var className = toStr(a)
+		if (className !== toStr(b)) return false
+		switch (className) {
+			// Strings, numbers, regular expressions, dates, and booleans are compared by value.
+			case '[object RegExp]':
+				// RegExps are coerced to strings for comparison (Note: '' + /a/i === '/a/i')
+			case '[object String]':
+				// Primitives and their corresponding object wrappers are equivalent; thus, `"5"` is
+				// equivalent to `new String("5")`.
+				return '' + a === '' + b
+			case '[object Number]':
+				// `NaN`s are equivalent, but non-reflexive.
+				// Object(NaN) is equivalent to NaN
+				if (+a !== +a) return +b !== +b
+				// An `egal` comparison is performed for other numeric values.
+				return +a === 0 ? 1 / +a === 1 / b : +a === +b
+			case '[object Date]':
+			case '[object Boolean]':
+				// Coerce dates and booleans to numeric primitive values. Dates are compared by their
+				// millisecond representations. Note that invalid dates with millisecond representations
+				// of `NaN` are not equivalent.
+				return +a === +b
+		}
+
+		var areArrays = className === '[object Array]'
+		if (!areArrays) {
+			if (typeof a != 'object' || typeof b != 'object') return false
+
+			// Objects with different constructors are not equivalent, but `Object`s or `Array`s
+			// from different frames are.
+			var aCtor = a.constructor
+			var bCtor = b.constructor
+			if (aCtor !== bCtor && !(isFn(aCtor) && aCtor instanceof aCtor &&
+					isFn(bCtor) && bCtor instanceof bCtor) && ('constructor' in a && 'constructor' in b)) {
+				return false
+			}
+		}
+		// Assume equality for cyclic structures. The algorithm for detecting cyclic
+		// structures is adapted from ES 5.1 section 15.12.3, abstract operation `JO`.
+		var length = aStack.length
+		while (length--) {
+			// Linear search. Performance is inversely proportional to the number of
+			// unique nested structures.
+			if (aStack[length] === a) return bStack[length] === b
+		}
+
+		// Add the first object to the stack of traversed objects.
+		aStack.push(a)
+		bStack.push(b)
+		var size, result
+		// Recursively compare objects and arrays.
+		if (areArrays) {
+			// Compare array lengths to determine if a deep comparison is necessary.
+			size = a.length
+			result = size === b.length
+			if (result) {
+				// Deep compare the contents, ignoring non-numeric properties.
+				while (size--) {
+					if (!(result = eq(a[size], b[size], aStack, bStack))) break
+				}
+			}
+		} else {
+			// Deep compare objects.
+			var keys = _.keys(a)
+			var key
+			size = keys.length
+			// Ensure that both objects contain the same number of properties before comparing deep equality.
+			result = _.keys(b).length === size
+			if (result) {
+				while (size--) {
+					// Deep compare each member
+					key = keys[size]
+					if (!(result = hasOwn(b, key) && eq(a[key], b[key], aStack, bStack))) break
+				}
+			}
+		}
+		// Remove the first object from the stack of traversed objects.
+		aStack.pop()
+		bStack.pop()
+		return result
+	};
+
+	// Perform a deep comparison to check if two objects are equal.
+	_.isEqual = function(a, b) {
+		return eq(a, b, [], [])
+	}
+
+	var $fn = $.fn
+
+	function Sync(dataModel, viewModel) {
+		this.dataModel = dataModel
+		this.viewModel = viewModel
+	}
+
+	Sync.prototype = {
+		refresh: function() {
+			var that = this
+			var viewModel = this.viewModel
+			each(this.dataModel, function(data, key) {
+				if (!(key in viewModel)) {
+					return
+				}
+				that.render(viewModel[key], data)
+			})
+		},
+		render: function(vm, data) {
+			if (_.isEqual(data, vm.lastValue)) {
+				return
+			}
+			vm.lastValue = typeof data === 'object' ? isArr(data) ? data.concat() : extend(true, {}, data) : data
+
+			var instance = vm.instance
+			var method = vm.method
+			var params = vm.params
+			var inProto = vm.method in $fn
+
+			if (inProto) {
+
+				if (isSameType(data)) {
+					var template = instance.eq(0).clone()
+					var frag = []
+					method = $fn[method]
+					each(data, function(value, index) {
+						var $item = instance.eq(index)
+						if (!$item.length) {
+							$item = template.clone()
+							frag.push($item.get(0))
+						}
+						method.apply($item, params.concat(value))
+					})
+					if (frag.length) {
+						instance.eq(-1).after(frag)
+						frag.push.apply(instance, frag)
+					}
+				} else {
+					$fn[method].apply(instance, params.concat(data))
+				}
+
+			} else {
+
+				if (!isFn(data)) {
+					return
+				}
+				method = data
+				params = this.dataModel[vm.method]
+				method.apply(instance, [].concat(params || []))
+			}
+		}
+	}
+
+
+	$.fn.refresh = function(dataModel) {
+		var that = this
+		if (isObj(dataModel)) {
+			new Sync(dataModel, this.scanView()).refresh()
+		} else if (isArr(dataModel)) {
+			each(dataModel, function(dm, index) {
+				new Sync(dm, that.eq(index).scanView()).refresh()
+			})
+		}
+		return this
+	}
 
 	//plus.js
-
 
 	extend($.fn, {
 
 		render: function(api) {
-			var self = this,
-				$fn = $.fn;
+			var that = this
+			var $fn = $.fn
 
-			function invoke(key, value) {
-				var $method = $fn[key];
-				$method && $method[isArray(value) ? 'apply' : 'call'](self, value);
+			function invoke(value, key) {
+				var $method = $fn[key]
+				isFn($method) && $method[isArr(value) ? 'apply' : 'call'](that, value)
 			}
 
 			if (isObj(api)) {
 
-				each(api, invoke);
+				each(api, invoke)
 
-			} else if (isArray(api)) {
+			} else if (isArr(api)) {
 
-				each(api, function() {
-					each(this, invoke);
-				});
+				each(api, function(theApi) {
+					if (isObj(theApi)) {
+						each(theApi, invoke)
+					}
+				})
 
 			}
 
-			return this;
+			return this
 		},
 
 		listen: function(model) {
-			var self = this;
-			self.refresh(model);
-			return $.observe(model).on(function(value, key) {
-				var o = {};
-				o[key] = value;
-				self.refresh(o);
-			});
+			var that = this
+			that.refresh(model)
+			return $.observe(model, function(value, key) {
+				var o = {}
+				o[key] = value
+				that.refresh(o)
+			})
 		}
 	});
 
 	$.define = function(name, callback) {
-		var target = $(name),
-			model;
-		if (!target.length) return;
-		model = {};
-		model = callback(model) || model;
-		return target.listen(model);
+		var target = $(name)
+		var model;
+		if (!target.length) return
+		model = {}
+		model = callback(model) || model
+		return target.listen(model)
 	};
 
 
 	$.render = function(models) {
 		$('[render]').each(function() {
-			var $this = $(this),
-				key = $this.attr('render'),
-				mod;
-			key in models && isObj(mod = models[key]) && $this.render(mod);
-		});
-		return this;
+			var $this = $(this)
+			var key = $this.attr('render')
+			var mod
+			key in models && isObj(mod = models[key]) && $this.render(mod)
+		})
+		return this
 	};
 
 	var $module = {
 		vmodel: {},
 
 		ready: function(callback) {
-			var self = this;
+			var self = this
 			$(document).ready(function() {
-				callback.call(self.$scan());
-			});
-			return this;
+				callback.call(self.$scan())
+			})
+			return this
 		},
 		$scan: function() {
-			var self = this;
+			var self = this
 			$('[app]').each(function() {
-				var $this = $(this),
-					appName = $this.attr('app'),
-					status = false;
-				self.vmodel[appName] = $this.getVM();
+				var $this = $(this)
+				var appName = $this.attr('app')
+				var status = false
+				self.vmodel[appName] = $this.scanView()
 				self.on(appName + '.module', function(model, appName) {
-					if (status || !isObj(model)) return;
-					status = true;
-					this[appName] = $this.listen(model);
-					status = false;
-				});
-			});
-			return this;
+					if (status || !isObj(model)) return
+					status = true
+					this[appName] = $this.listen(model)
+					status = false
+				})
+			})
+			return this
 		}
-	};
+	}
 
-	$.module = extend($.observe({}), $module);
+	$.module = $.observe($module)
 
 }(this);
